0171932ef8f5bb8f1f69612bcdcf5103d1957e8a
Amie  Morales <xamiemmorales@gmail.com>
2024-09-20 11:42:02 -0500

    firebase setup

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
+// src/config/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-tCiTSHZ9JnCRUAa0soDgj5mJbC2iLBc",
-  authDomain: "amie-s-crm.firebaseapp.com",
-  projectId: "amie-s-crm",
-  storageBucket: "amie-s-crm.appspot.com",
-  messagingSenderId: "495739432372",
-  appId: "1:495739432372:web:0e3f5feaca936a8af456e1",
-  measurementId: "G-Y14JWH6YNC",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
